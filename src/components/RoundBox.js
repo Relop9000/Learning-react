93ccbf9480530f87ca85fde4f6be308ec6a0acf7
Relop9000 <amunkhorgil03@gmail.com>
2024-08-14 12:33:28 +0800

    commit

--- src/components/RoundBox.js
+++
@@ -1,6 +0,0 @@
-import styles from "../styles/Round.module.css";
-
-export default function Button(props) {
-  const { text } = props;
-  return <div className={styles.round}>{text}</div>;
-}
---
+++ src/components/UserProfile.js
@@ -0,0 +1,5 @@
+import styles from "../styles/UserProfile.module.css";
+
+export default function UserProfile() {
+  return <img className={styles.UserProfile}></img>;
+}
